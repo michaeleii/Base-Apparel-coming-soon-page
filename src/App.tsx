1f1185761d97a0173ca14d5f1652db96601d8878
Michael Lei <michaelleie0ca@gmail.com>
2023-07-17 10:04:51 -0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Logo from "./components/Logo";
 
 function App() {
   return (
-    <div className="relative mx-auto grid min-h-screen w-full  grid-cols-1 text-[hsl(0_36%_70%)] xl:grid-cols-[1fr_610px] xl:grid-rows-[auto_1fr]  xl:before:absolute xl:before:top-0 xl:before:h-screen xl:before:w-full xl:before:bg-bg-pattern xl:before:bg-no-repeat xl:before:object-cover">
-      <div className="z-10 p-10 xl:pl-20 xl:pt-20">
+    <div className="relative mx-auto grid h-screen w-full  grid-cols-1 text-[hsl(0_36%_70%)] xl:grid-cols-2 xl:grid-rows-[auto_1fr]  xl:before:absolute xl:before:top-0 xl:before:min-h-screen xl:before:min-w-full xl:before:bg-bg-pattern xl:before:bg-contain xl:before:bg-no-repeat">
+      <div className="z-10 p-10 xl:pl-40 xl:pt-20">
         <Logo />
       </div>
       <picture className="z-10 xl:row-span-2">
@@ -15,7 +15,7 @@ function App() {
           className="w-full object-cover xl:h-full"
         />
       </picture>
-      <div className="z-10 p-10 text-center xl:max-w-2xl xl:pl-20 xl:pt-20 xl:text-left">
+      <div className="z-10 p-10 text-center xl:max-w-2xl xl:pl-40 xl:pt-20 xl:text-left">
         <h1 className="mb-3 mt-10 text-5xl font-light uppercase leading-tight tracking-[0.2em] xl:text-7xl">
           We're{" "}
           <span className="font-semibold text-[hsl(0_6%_24%)]">
